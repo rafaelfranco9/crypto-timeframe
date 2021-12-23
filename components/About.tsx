ec12f52159c6df9392c5f69b390d7cc8e26dc5ec
Rafael Franco <francosprivato@gmail.com>
2021-12-23 14:06:12 -0500

    fixing text

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
         difference between the start-price and end-price is positive or
         negative. <br/>Each card will have a different type of a bull or bear
         depending on the <b className="text-main">price change percentage</b> (percentage details in the next
-        section). <br/>Each card have <b className="text-main">energy</b>, this energy is calculated taking into
+        section). <br/><br/>Each card have <b className="text-main">energy</b>, this energy is calculated taking into
         account the Market Cap change, the number of days and the power of the
         Bull/Bear. <br/>The energy formula is: <b className="text-main">( Market Cap change / 1,000,000 * Days
         * Bull/Bear Power )</b>
--- components/Value.tsx
+++ components/Value.tsx
@@ -9,12 +9,12 @@ const Value = () => {
         for now, but we plan to have more cryptocurrencies that our community
         demands. All crypto timeframe card owners will have priority in every
         release of new cryptocurrencies time frames. For cryptocurrencies that
-        have been already release, we will get out new timeframes yearly. <br />{" "}
+        have been already release, we will get out new timeframes yearly. <br /><br />
         The <b className="text-main">value</b> we see as a team on this cards is that you are not just buying
         a unique crypto time frame card, you are buying history! Just imagine
         the million of feelings involve on those charts, all that hope, fear,
         anxiety, happiness, etc. that’s what really empowers theses cards,
-        that’s what they truly represent. <br /> CryptoTimeFrame is one of many
+        that’s what they truly represent. <br /><br/> CryptoTimeFrame is one of many
         other projects that will come in the future in the NYOI ecosystem and
         they will be all interconnected, meaning that if you are part of our
         community it will be a pleasure for us to give you priority and more
